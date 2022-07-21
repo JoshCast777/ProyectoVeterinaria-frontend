46f8e154a1256fac134ea3e9c6d455f0354afbff
KAVCH <kevinchoez1234@gmail.com>
2022-07-21 14:02:36 -0500

    Cambio en la seccion de nosotros

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -8,9 +8,11 @@ import { ServicesComponent } from "./services/services.component";
 import { ServicesContentComponent } from './services/services-content/services-content.component';
 import { HeaderHomeComponent } from "./home/header-home/header-home.component";
 import { MainHomeComponent } from "./home/main-home/main-home.component";
+import { HeaderAboutComponent } from './about/header-about/header-about.component';
+import { MainAboutComponent } from './about/main-about/main-about.component';
 
 @NgModule({
-	declarations: [HomeComponent, AboutComponent, ServicesComponent, ServicesContentComponent, HeaderHomeComponent, MainHomeComponent],
+	declarations: [HomeComponent, AboutComponent, ServicesComponent, ServicesContentComponent, HeaderHomeComponent, MainHomeComponent, HeaderAboutComponent, MainAboutComponent],
 	imports: [CommonModule, SharedModule]
 })
 export class PagesModule {}
